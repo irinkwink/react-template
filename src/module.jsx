e069609cdf82de23a1f031f1521080a40dc3617d
irinkwink <irina-ns@yandex.ru>
2022-07-15 09:41:06 +0300

    feat: add config and base layout

---
+++ src/module.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import okSign from "./img/ok.svg";
+
+const Module = () => (
+  <>
+    <h1>Hello, React!</h1>
+    <img src={okSign} alt="ok" width={70}/>
+
+  </>
+);
+
+export default Module;
